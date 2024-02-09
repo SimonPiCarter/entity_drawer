278182d11f98a8ef02ed89a690b336edd215315f
SimonP <SimonP@gmail.com>
2024-02-09 22:56:01 +0100

    removed includes

--- src/src/manager/levels/test/TestLevel.h
+++ src/src/manager/levels/test/TestLevel.h
@@ -1,26 +1,5 @@
 #pragma once
 
-
-// #include "controller/trigger/Listener.hh"
-// #include "command/entity/EntityAttackMoveCommand.hh"
-// #include "command/spawner/AreaSpawnerCommand.hh"
-// #include "state/entity/Building.hh"
-// #include "state/entity/Unit.hh"
-// #include "state/entity/Resource.hh"
-// #include "state/model/entity/BuildingModel.hh"
-// #include "state/vision/PatternHandler.hh"
-// #include "state/State.hh"
-// #include "step/command/CommandQueueStep.hh"
-// #include "step/entity/spawn/UnitSpawnStep.hh"
-// #include "step/entity/spawn/ResourceSpawnStep.hh"
-// #include "step/entity/spawn/BuildingSpawnStep.hh"
-// #include "step/player/PlayerSpawnStep.hh"
-// #include "step/player/PlayerSpendResourceStep.hh"
-// #include "step/state/StateWinStep.hh"
-// #include "step/trigger/TriggerSpawn.hh"
-// #include "step/team/TeamVisionStep.hh"
-// #include "utils/Binary.hh"
-
 #include "library/Library.hh"
 #include "step/Step.hh"
 
